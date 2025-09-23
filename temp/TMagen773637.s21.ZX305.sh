81a710defe4bcaee599923811f4f20c548845eb7
cyberdayag <your_email@example.com>
2025-09-23 13:54:30 -0400

    Partial update: enumeration section logic

--- temp/TMagen773637.s21.ZX305.sh
+++ temp/TMagen773637.s21.ZX305.sh
@@ -437,33 +437,21 @@ ENUMERATION_MODE_SELECTOR() {
         # Run the corresponding enumeration function based on user choice
         if [[ "$user_choice" =~ ^[Bb]$ ]]; then
             echo -e "\n\e[31m[!]\e[0m\e[32m Starting BASIC ENUMERATION...\e[0m"
-
-            (
-                BASIC_ENUMERATION
-
-                # Convert the *.txt into a PDF
-                for txt_file in "$host_dir"/enumeration_*.txt; do
+            BASIC_ENUMERATION
+            for txt_file in "$host_dir"/enumeration_*.txt; do
                 pdf_file="${txt_file%.txt}.pdf"
                 (enscript -B -f Courier10 -p - "$txt_file" | ps2pdf - "$pdf_file") > /dev/null 2>&1
-                done
-            ) &
-            SPINNER $!
+            done
             break
 
         elif [[ "$user_choice" =~ ^[Ii]$ ]]; then
             echo -e "\n\e[31m[!]\e[0m\e[32m Starting INTERMEDIATE ENUMERATION...\e[0m"
-
-            (
-                BASIC_ENUMERATION
-                INTERMEDIATE_ENUMERATION
-
-                # Convert the *.txt into a PDF
-                for txt_file in "$host_dir"/enumeration_*.txt; do
-                    pdf_file="${txt_file%.txt}.pdf"
-                    (enscript -B -f Courier10 -p - "$txt_file" | ps2pdf - "$pdf_file") > /dev/null 2>&1
-                done
-            ) &
-            SPINNER $!
+            INTERMEDIATE_ENUMERATION
+            # Convert the *.txt into a PDF
+            for txt_file in "$host_dir"/enumeration_*.txt; do
+                pdf_file="${txt_file%.txt}.pdf"
+                (enscript -B -f Courier10 -p - "$txt_file" | ps2pdf - "$pdf_file") > /dev/null 2>&1
+            done
             break
 
         elif
@@ -473,10 +461,10 @@ ENUMERATION_MODE_SELECTOR() {
             echo -e "   ⚠️  ATTENTION: Advanced Enumeration Requirements  ⚠️"
             echo -e "============================================================"
             echo -e " Userlist AND Password List are REQUIRED."
-            echo -e " - If no custom password list is provided, rockyou.txt will be used."
             echo -e " - If no custom userlist is provided, default userlist will be used."
+            echo -e " - If no custom password list is provided, rockyou.txt will be used."
             echo -e "============================================================\e[0m\n"
-
+            sleep 2
             
             # Prompt the user to enter the full path to a  userlist
             read -r -p $'\e[34m[?]\e[0m Enter full path to userlist or press enter for default userlist: ' custom_userlist
@@ -505,19 +493,13 @@ ENUMERATION_MODE_SELECTOR() {
             
             # Inform the user that Advanced Enumeration is starting
             echo -e "\n\e[31m[!]\e[0m\e[32m Starting ADVANCE ENUMERATION...\e[0m"
+            ADVANCED_ENUMERATION
 
-            (
-                BASIC_ENUMERATION
-                INTERMEDIATE_ENUMERATION
-                ADVANCED_ENUMERATION
-            
-             # Convert the *.txt into a PDF
+            # Convert the *.txt into a PDF
             for txt_file in "$host_dir"/enumeration_*.txt; do
                 pdf_file="${txt_file%.txt}.pdf"
                 (enscript -B -f Courier10 -p - "$txt_file" | ps2pdf - "$pdf_file") > /dev/null 2>&1
             done
-            ) &
-            SPINNER $!
             break
         else
             echo -e "\e[91m[!] Wrong choice. Example: B, I or A\e[0m"
@@ -534,11 +516,18 @@ ENUMERATION_MODE_SELECTOR() {
 # detects Domain Controllers and DHCP servers, logs findings to TXT, and converts logs to PDF.
 BASIC_ENUMERATION() {
 
+    local host_dir="$working_dir/$host"
+
     for ip in $(cat "$working_dir/live_hosts.txt"); do
         echo -e "\e[31m[*]\e[0m\e[32m Enumeration: ${ip}\e[0m"
         
         # Define TXT file for this host
         enumeration_result_txt="$host_dir/enumeration_${ip}.txt"
+        
+        # Ensure TXT file exists; if not, create it with a header
+        if [ ! -f "$enumeration_result_txt" ]; then
+            echo -e "\n INTERMEDIATE ENUMERATION RESULT FOR ${ip}\n" > "$enumeration_result_txt"
+        fi
 
         # Write header
         echo -e "\n BASIC ENUMERATION RESULT FOR ${ip}\n" >> "$enumeration_result_txt" 2>/dev/null
@@ -564,22 +553,22 @@ BASIC_ENUMERATION() {
             echo -e "\n$msg" >> "$enumeration_result_txt" 2>/dev/null
             echo -e "\e[33m$msg\e[0m"
         fi
-
-        
-         # Automatically determine the active network interface
-        interface=$(ip -o -4 addr show up | awk '!/lo/ {print $2; exit}')
-        
-        # DHCP server detection
-        dhcp_output=$(nmap -e "$interface" --script broadcast-dhcp-discover 2>/dev/null)
-
-        # Check if a DHCP server was found and log it
-        if echo "$dhcp_output" | grep -q "Server Identifier:"; then
-            dhcp_ip=$(echo "$dhcp_output" | grep "Server Identifier:" | awk -F ':' '{print $NF}' | tr -d ' ')
-            msg="[+] DHCP SERVER DETECTED ${dhcp_ip}"
-            echo -e "\n$msg" >> "$enumeration_result_txt" 2>/dev/null
-            echo -e "\e[33m$msg\e[0m"
-        fi
     done
+
+        
+     # Automatically determine the active network interface
+    interface=$(ip -o -4 addr show up | awk '!/lo/ {print $2; exit}')
+        
+    # DHCP server detection
+    dhcp_output=$(nmap -e "$interface" --script broadcast-dhcp-discover 2>/dev/null)
+
+    # Check if a DHCP server was found and log it
+    if echo "$dhcp_output" | grep -q "Server Identifier:"; then
+        dhcp_ip=$(echo "$dhcp_output" | grep "Server Identifier:" | awk -F ':' '{print $NF}' | tr -d ' ')
+        msg="[+] DHCP SERVER DETECTED ${dhcp_ip}"
+        echo -e "\n$msg" >> "$enumeration_result_txt" 2>/dev/null
+        echo -e "\e[33m$msg\e[0m"
+    fi    
 }
 
 
@@ -588,27 +577,70 @@ BASIC_ENUMERATION() {
 # Performs intermediate host enumeration: expands on BASIC_ENUMERATION results for each live host.
 # This function appends results to the TXT file already created in BASIC_ENUMERATION.
 INTERMEDIATE_ENUMERATION() {
+    
+    local host_dir="$working_dir/$host"
 
     for ip in $(cat "$working_dir/live_hosts.txt"); do
         echo -e "\e[31m[*]\e[0m\e[32m Enumeration: ${ip}\e[0m"
-
+        
         # Define TXT file for this host
         enumeration_result_txt="$host_dir/enumeration_${ip}.txt"
-                
+
         # Write header
         echo -e "\n INTERMEDIATE ENUMERATION RESULT FOR ${ip}\n" >> "$enumeration_result_txt" 2>/dev/null
 
-        # Define ports for intermediate scanning
-        ports=21,22,139,445,389,636,3389,5985,5986
+        # TCP enumeration
+        if [ "$(grep -c '/tcp' "$scan_result_txt")" -gt 0 ]; then
+            tcp_ports=$(grep '/tcp' "$scan_result_txt" | awk -F'/' '{print $1}' | paste -sd, -)
+            nmap -Pn -sV -p "$tcp_ports" ${ip} | grep -E '^PORT|/tcp|open' >>  "$enumeration_result_txt" 2>/dev/null
+        fi
 
-        # Run Nmap scripts:
-        #   - vuln: check common vulnerabilities
-        #   - smb-os-discovery: detect SMB server OS
+        # UDP enumeration
+        if [ "$(grep -c '/udp' "$scan_result_txt")" -gt 0 ]; then
+            udp_ports=$(grep '/udp' "$scan_result_txt" | awk -F'/' '{print $1}' | paste -sd, -)
+            nmap -Pn -sV -p "$udp_ports" ${ip} | grep -E '^PORT|/tcp|open' >>  "$enumeration_result_txt" 2>/dev/null
+        fi
+
+        # Define ports for intermediate scanning
+        # These ports are commonly used for services relevant to enumeration:
+        # 21 (FTP), 22 (SSH), 139/445 (SMB), 389/636 (LDAP), 3389 (RDP), 5985/5986 (WinRM)
+        ports=21,22,139,445,389,636,3389,5985,5986
         #   - smb-enum-shares: enumerate SMB shares
         #   - smb-enum-users: enumerate SMB users
-        nmap -Pn -sV -p "$ports" --script vuln,smb-os-discovery,smb-enum-shares,smb-enum-users "${ip}" | awk 'NR > 3'>> "$enumeration_result_txt" 2>/dev/null
+        # Skip the first three lines of nmap output (header) for cleaner results in the TXT file
+        nmap -Pn -sV -p "$ports" --script smb-os-discovery,smb-enum-shares,smb-enum-users,ldap-search,smb-enum-domains,nbstat "${ip}" | awk 'NR > 3'>> "$enumeration_result_txt" 2>/dev/null
+      
+
+        # Domain Controller detection
+        if grep -q "ldap.*Active Directory" "$enumeration_result_txt" \
+            && grep -q "88/tcp" "$enumeration_result_txt" \
+            && grep -q "3268/tcp" "$enumeration_result_txt"; then
+
+            msg="[+] DOMAIN CONTROLLER DETECTED ${ip}"
+            echo -e "\n$msg" >> "$enumeration_result_txt" 2>/dev/null
+            echo -e "\e[33m$msg\e[0m"
+        fi
+
+        
+         # Automatically determine the active network interface
+        interface=$(ip -o -4 addr show up | awk '!/lo/ {print $2; exit}')
+        
+        # DHCP server detection
+        dhcp_output=$(nmap -e "$interface" --script broadcast-dhcp-discover 2>/dev/null)
+
+
     done
+
+            # Check if a DHCP server was found and log it
+        if echo "$dhcp_output" | grep -q "Server Identifier:"; then
+            dhcp_ip=$(echo "$dhcp_output" | grep "Server Identifier:" | awk -F ':' '{print $NF}' | tr -d ' ')
+            msg="[+] DHCP SERVER DETECTED ${dhcp_ip}"
+            echo -e "\n$msg" >> "$enumeration_result_txt" 2>/dev/null
+            echo -e "\e[33m$msg\e[0m"
+        fi
 }
+
+
 
 
 
